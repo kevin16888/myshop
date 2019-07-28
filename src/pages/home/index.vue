7be5b1a910d123cfbbad21769a6a97d9756af764
kevin16888 <1161276621@qq.com>
2019-07-28 19:25:18 +0800

    完成楼层效果

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -10,13 +10,34 @@
     <!-- 轮播图 -->
     <swiper indicator-dots="true">
       <swiper-item :key="item.goods_id" v-for="item in swiper">
-        <image :src="item.image_src" class="slide-image" />
+        <image :src="item.image_src" class="slide-image"/>
       </swiper-item>
     </swiper>
     <!-- 菜单 -->
     <div class="menu">
       <div :key="i" v-for="(item,i) in menu" class="menu-item">
         <img :src="item.image_src">
+      </div>
+    </div>
+    <!-- 商品列表 -->
+    <div class="floor" :key="i" v-for="(item,i) in floor">
+      <!-- 楼层的头部 -->
+      <div class="floor-title">
+        <img :src="item.floor_title.image_src" mode="aspectFill">
+      </div>
+      <div class="floor-content">
+        <div class="left">
+          <img :src="item.product_list[0].image_src" mode="aspectFill">
+        </div>
+        <div class="right">
+          <img
+            v-if="index > 0"
+            :key="index"
+            v-for="(img,index) in item.product_list"
+            :src="img.image_src"
+            mode="aspectFill"
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -28,7 +49,8 @@ export default {
   data () {
     return {
       swiper: [],
-      menu: []
+      menu: [],
+      floor: []
     }
   },
   methods: {
@@ -66,11 +88,16 @@ export default {
       // let res = await request('home/catitems')
       // this.menu = res.data.message
       this.menu = await this.queryData('home/catitems')
+    },
+    async floorData () {
+      // 楼层数据
+      this.floor = await this.queryData('home/floordata')
     }
   },
   mounted () {
     this.swiperData()
     this.menuData()
+    this.floorData()
   }
 }
 </script>
@@ -94,5 +121,40 @@ export default {
 .menu .menu-item img {
   width: 128rpx;
   height: 140rpx;
+}
+.floor {
+  margin-top: 20rpx;
+}
+.floor-title {
+  width: 100%;
+}
+.floor-title img {
+  width: 100%;
+  height: 60rpx;
+  display: block;
+}
+.floor-content {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 20rpx;
+  box-sizing: border-box;
+}
+.floor-content .left img {
+  width: 232rpx;
+  height: 385rpx;
+  border-radius: 4px;
+}
+.floor-content .right {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-left: 14rpx;
+}
+.floor-content .right img {
+  width: 232rpx;
+  height: 188rpx;
+  border-radius: 4px;
 }
 </style>
