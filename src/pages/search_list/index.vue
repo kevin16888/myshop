3f8e10f4b6e0244ca8990f60c856c9e3b7fdb58d
kevin16888 <1161276621@qq.com>
2019-07-30 17:54:59 +0800

    数据加载完成的控制逻辑

--- src/pages/search_list/index.vue
+++ src/pages/search_list/index.vue
@@ -25,12 +25,14 @@
         <div class="goods-right">
           <h4>{{item.goods_name}}</h4>
           <div class="price">
-            <span>￥</span>
+            <span>¥</span>
             {{item.goods_price}}
           </div>
         </div>
       </navigator>
     </div>
+    <!-- 提示没有更多数据了 -->
+    <div class="more" v-if="hasMore">没有更多数据了</div>
   </div>
 </template>
 
@@ -47,7 +49,8 @@ export default {
       pagenum: 1,
       total: 0,
       // 保证接口调用完成之后才可以再次调用接口，如果接口正在获取数据，那么在这个过程中不允许再次触发接口调用
-      isLoading: false
+      isLoading: false,
+      hasMore: false
     }
   },
   methods: {
@@ -56,11 +59,12 @@ export default {
       this.currentIndex = index
     },
     async loadData () {
-      // 判断是否已经加载完成
-      if (this.isLoading) {
+      // 如果没有更多数据，就应该禁止发送请求调用接口
+      // 本次接口调用是否已经加载完成
+      if (this.isLoading || this.hasMore) {
         return
       }
-      //作用为：禁止再次触发接口调用
+      // 作用为：禁止再次触发接口调用
       this.isLoading = true
       // 根据关键字加载匹配的商品列表数据
       let res = await request('goods/search', 'get', {
@@ -75,6 +79,11 @@ export default {
       // 返回的pagenum为字符串，需转化为数值
       this.pagenum = parseInt(message.pagenum)
       this.total = message.total
+      // 判断是否还有更多数据
+      if (this.list.length >= this.total) {
+        // 没有更多数据了
+        this.hasMore = true
+      }
       // 加载完成数据之后，让页码加1
       this.pagenum = this.pagenum + 1
       // 接口数据返回之后，才允许再次发出请求
